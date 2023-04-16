393870a8b50b05f305f6f18cae1f6f74573b39d2
den <denissytnikmacho@gmail.com>
2023-04-16 15:20:28 +0300

    added code for starting the server

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+const connectDB = require('./config/dbConn');
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
+
+// Global variables
+const PORT = 3500;
+
+// Connect to db
+connectDB();
+
+// Middleware for cookies
+app.use(cookieParser());
+
+// Built-in middleware for json
+app.use(express.json());
+
+// Listen ports
+mongoose.connection.once('open', () => {
+  console.log('Connected to MongoDB');
+  app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+});
