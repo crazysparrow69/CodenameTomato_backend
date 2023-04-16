778ba45c9f2f267e16ecc8eaa66c8328db1d687b
den <denissytnikmacho@gmail.com>
2023-04-16 16:00:27 +0300

    added function for creating tasks

---
+++ controllers/taskController.js
@@ -0,0 +1,25 @@
+const Task = require("../model/Task");
+
+const createTask = async (req, res) => {
+  const { title, description, deadline, date } = req.body;
+
+  if (title.length < 3 || description.length < 3 || deadline === "" || date === "") {
+    res.sendStatus(400);
+  }
+
+  try {
+    const result = await Task.create({
+      title,
+      description,
+      deadline,
+      date
+    });
+
+    console.log(result);
+    res.status(201).json({ "message": "New task created" });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+module.exports = { createTask };
