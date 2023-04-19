6a873b501f133fa10bba31cf1d8f8bdcc0fe5366
den <denissytnikmacho@gmail.com>
2023-04-19 17:35:22 +0300

    removed date field from schema, added timestamps

--- model/Task.js
+++ model/Task.js
@@ -1,27 +1,28 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const taskSchema = new Schema({
-  userId: {
-    type: String,
-    required: true,
+const taskSchema = new Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    deadline: {
+      type: String,
+      required: true,
+    },
   },
-  title: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  deadline: {
-    type: String,
-    required: true,
-  },
-  date: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("task", taskSchema);
